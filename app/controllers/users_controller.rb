a6a5d6b0ed8963a9cfb4e133ef0e9765ddd6ef73
1543rrr <73112533+1543rrr@users.noreply.github.com>
2021-05-12 22:41:15 +0900

    ユーザーcrud機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @dishes = @user.dishes.paginate(page: params[:page], per_page: 5)
-    @log = Log.new
+    # @dishes = @user.dishes.paginate(page: params[:page], per_page: 5)
+    # @log = Log.new
   end
 
   def new
@@ -40,6 +40,43 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if current_user.admin?
+      @user.destroy
+      flash[:success] = "ユーザーの削除に成功しました"
+      redirect_to users_url
+    elsif current_user?(@user)
+      @user.destroy
+      flash[:success] = "自分のアカウントを削除しました"
+      redirect_to root_url
+    else
+      flash[:danger] = "他人のアカウントは削除できません"
+      redirect_to root_url
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+
+  def user_params_update
+    params.require(:user).permit(:name, :email, :introduction, :sex)
+  end
+
+  
+def correct_user
+    @user = User.find(params[:id])
+    if !current_user?(@user)
+      flash[:danger] = "このページへはアクセスできません"
+      redirect_to(root_url)
     end
   end
 end
